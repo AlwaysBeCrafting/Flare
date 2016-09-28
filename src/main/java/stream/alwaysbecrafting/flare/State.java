854fbd4b6cd913a2c4dff20a9955e0b5d27e6e70
Alexander Prime <rainesabaal@gmail.com>
2016-09-28 07:40:37 -0700

    Add javadocs

--- src/main/java/stream/alwaysbecrafting/flare/State.java
+++ src/main/java/stream/alwaysbecrafting/flare/State.java
@@ -4,23 +4,44 @@ package stream.alwaysbecrafting.flare;
 public class State {
 	//--------------------------------------------------------------------------
 
+	/**
+	 * A state with no operations defined
+	 */
 	public static final State EMPTY = new State() {};
 
 	//--------------------------------------------------------------------------
 	//--------------------------------------------------------------------------
 
+	/**
+	 * Override to handle a {@link StateMachine} switching to this state
+	 *
+	 * @param params The parameters given to {@link StateMachine#change(String, Object...)}
+	 */
 	public void onEnter( Object... params ) {}
 
 	//--------------------------------------------------------------------------
 
+	/**
+	 * Override to handle update calls on a {@link StateMachine}
+	 *
+	 * @param deltaTime The time given to {@link StateMachine#update(double)}; ostensibly, the time
+	 *                  between the previous loop and the current one
+	 */
 	public void onUpdate( double deltaTime ) {}
 
 	//--------------------------------------------------------------------------
 
+	/**
+	 * Override to handle input.
+	 */
 	public void onHandleInput() {}
 
 	//--------------------------------------------------------------------------
 
+	/**
+	 * Override to handle a {@link StateMachine} switching to another state from
+	 * this one
+	 */
 	public void onExit() {}
 
 	//--------------------------------------------------------------------------
